b9b85bf83636232a21100e7aa469402fe1c6d2fe
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2024-10-07 07:25:50 -0400

    Comienza Tesalonicenses 2

--- herram/gv.sh
+++ herram/gv.sh
@@ -2,7 +2,7 @@
 # Edita
 
 #TERM=xterm-color xterm -geometry 80x50 -e "gvim corintios2.gbfxml" &
-gvim tesalonicenses1.gbfxml &
-gvim ref/sword_kjv/I_Thessalonians-2023-01-06.osis.xml %
+gvim tesalonicenses2.gbfxml &
+gvim ref/sword_kjv/II_Thessalonians-2023-01-06.osis.xml %
 
 
