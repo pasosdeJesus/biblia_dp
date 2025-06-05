d4e96b06a44390d234ddf3b033608743a7d1a93d
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2025-06-05 03:05:21 -0400

    Mejora script pub

--- herram/rsync.sh
+++ herram/rsync.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
-#
+# Genera y publica en traduccion.pasosdeJesus.org
+
+rm -rf html/*
+make multi
+echo "Generado. Presione [ENTER] para publicar"
+read
 rsync -ravz --delete html/ traduccion.pasosdeJesus.org:/var/www/pasosdeJesus/traduccion/
