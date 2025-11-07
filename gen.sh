90377a8fb9589a0ef3098682cbb80bcac9e608b0
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2025-11-07 11:46:02 -0500

    regenera

--- gen.sh
+++ gen.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 # Genera archivos en otros formatos a partir de gbfxml
 
-(cd gen/capitulos; ./partes.sh)
+(cd gen/capitulos; ./parte.sh)
 trad=`grep -l "wi type" *gbfxml`
 (for i in $trad; do  n=`echo $i | sed -e "s/.gbfxml//"`; echo $n; ./gbfxml2usfm.sh $i gen/usfm; done)
+node herram/extmorf.js
